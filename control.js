9c84eabb72512bc5213aef4e25c49ab8294ab96c
ChristopherVizweet <cris.5abril2019@gmail.com>
2022-10-03 20:13:27 -0500

    primer commit

---
+++ control.js
@@ -0,0 +1,9 @@
+function abrir(){
+    document.getElementById("wind").style.display="block";
+    }
+    function cerrar(){
+        document.getElementById("wind").style.display="none";
+    }
+    function subir(){
+        document.getElementById("volumen").style.display="none";
+    }
